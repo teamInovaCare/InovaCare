ba52e583eaa64bc3923c51022385010b48ece3c4
InovaCare <inovacareini@gmail.com>
2025-10-17 16:58:44 +0000

    O que faltou

--- app/routes/routerProf.js
+++ app/routes/routerProf.js
@@ -172,6 +172,13 @@ routerProf.get("/prontugeral", function (req, res) {//prontuario profissional
 
 
 
+/**Minhas consultas */
+routerProf.get("/consultas-prof", function (req, res) {/**Minhas consultas */
+    res.render("pages/consultas-prof.ejs")
+});
+
+
+
 
 
 
